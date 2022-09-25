ea121c20d51a94e442a3435d90e106036cb8d297
arian_heidari <96712618+arianhei@users.noreply.github.com>
2022-09-25 11:20:56 +0330

    networking

--- scifi-fps/Assets/Scripts/Networking/Lobby/Network Manager.cs
+++ scifi-fps/Assets/Scripts/Networking/Lobby/Network Manager.cs
@@ -1,18 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
-
-public class NetworkManager : MonoBehaviour
+public class NetworkManager : MonoBehaviourPunCallbacks
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        PhotonNetwork.ConnectUsingSettings();   
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnConnectedToMaster()
     {
-        
+        Debug.Log("Connected");
     }
 }
